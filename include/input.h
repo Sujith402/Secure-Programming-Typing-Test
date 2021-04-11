4346183299d96cc64b049b460e89bd1fdb844482
Sujith <sujith402@gmail.com>
2021-04-11 11:19:03 +0530

    Added backspace and basic input sanitization

--- include/input.h
+++ include/input.h
@@ -1,8 +1,18 @@
+#pragma once
+
 #include <ncurses.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 #include "queue.h"
 
 #define CORRECT 2
 #define WRONG 1
+#define BACK 3
+
 
 void Take_Input(WINDOW *win, Queue *q);
+void Handle_Backspace(int *res,int *col, int *word_no, int *curr, Queue *q);
+void Handle_Space(Queue *q, int res, int word_no, int letter);
+int Is_Ok(int ch);
 void Init_Colour();
--- src/terminal.c
+++ src/terminal.c
@@ -24,6 +24,9 @@ WINDOW *Init_Local_Window(WIN *win) {
     win->border.tl = win->border.tr = '+';
     win->border.bl = win->border.br = '+';
     WINDOW *local_win = newwin(win->height,win->width,win->starty,win->startx);
+
+    //Allow for detecting function key presses when getting input in the window
+    keypad(local_win,true);
     return local_win;
 }
 
@@ -47,11 +50,11 @@ void Display_Box(WINDOW *win, WIN *win_props, bool visible) {
 }
 
 void Display_Text(WINDOW *win,WIN *winProps, Queue *q) {
-    int x = 0;
-    int y = 0;
+    int x = 1;
+    int y = 1;
     int xMax = winProps->width - 1;
     int yMax = winProps->height - 1;
-    wmove(win,1,1);
+    wmove(win,y,x);
     for (int i=q->start; i<q->size; i++) {
         if (x+q->words[i].len - 1 < xMax) {
             wprintw(win,q->words[i].w);
@@ -59,7 +62,7 @@ void Display_Text(WINDOW *win,WIN *winProps, Queue *q) {
         }
         else {
             if (y+1 <= yMax) {
-                y+=2;
+                y++;
                 x = 1;
                 mvwprintw(win,y,x,q->words[i].w);
                 x+=q->words[i].len;
@@ -69,34 +72,6 @@ void Display_Text(WINDOW *win,WIN *winProps, Queue *q) {
     }
     wmove(win,1,1);
     wrefresh(win);
-}
-
-
-
-//To use later
-//void Change_Colour() {
-//    if (!has_colors()) {
-//        endwin();
-//        printf("Does not support colours\n");
-//        exit(1);
-//    }
-//    start_color();
-//    init_pair(1,COLOR_RED,COLOR_BLACK);
-//    attron(COLOR_PAIR(1));
-//    print_line(stdscr, 10, 0, COLS, "Coloured line");
-//    attroff(COLOR_PAIR(1));
-//
-//}
-
-void print_line(WINDOW *win, int starty, int startx, int width, char *str) {
-    int length;
-    float offset;
-
-    length = strlen(str);
-    offset = (width - length)/2;
-    int x = startx + (int) offset;
-    mvwprintw(win, starty, x, "%s", str);
-    refresh();
 }
 
 void Exit_Terminal() {
